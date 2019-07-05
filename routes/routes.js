ff63fe25ea3cab5b7258a9718cdc9f2a1b706f2e
Sam Ilkov <samuil.ilkov90@gmail.com>
2019-07-05 17:54:40 -0400

    added header auth

--- routes/routes.js
+++ routes/routes.js
@@ -13,7 +13,7 @@ var appRouter = function (app) {
 
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token");
         res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
         next();
     });
