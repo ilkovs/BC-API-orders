23d17012e73eab6145d56f86c194cf2d2c481e4b
Sam Ilkov <samuil.ilkov90@gmail.com>
2019-07-05 16:23:34 -0400

    not able to update order status id

--- routes/routes.js
+++ routes/routes.js
@@ -14,6 +14,7 @@ var appRouter = function (app) {
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
         next();
     });
 
